32006ac0070ee563ff1cdd77e32949417621a8f2
ravichandrazysk <ravichandra.bk@zysk.tech>
2024-12-19 13:59:14 +0530

    feat: dep 4

--- src/app/email-verified/page.tsx
+++ src/app/email-verified/page.tsx
@@ -3,7 +3,8 @@ import React from "react";
 import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-
+import dynamic from "next/dynamic";
+const Lottie = dynamic(() => import("react-lottie-player"));
 const Page = () => {
   const router = useRouter();
   return (
@@ -13,6 +14,12 @@ const Page = () => {
         className=" min-h-screen bg-slate-500 flex flex-col  h-full items-center justify-center"
       >
         <Card className="flex min-h-96 max-w-lg items-center justify-center  flex-col">
+          <Lottie
+            loop
+            path="/lotties/account-verified.json"
+            play
+            style={{ width: "40%" }}
+          />
           <p className="text-xl font-medium">Email verified successfully!</p>
           <Button
             className="max-w-sm w-full font-semibold text-base bg-[#ff851b] hover:bg-[#ff851b]/90 !mt-8 h-12"
